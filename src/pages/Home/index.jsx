23568066bb684c947d8fd1f0a082dd63d8815831
GuiLeoni14 <guileone167@gmail.com>
2022-02-03 13:30:23 -0200

    ajustando estrutura

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import Container from '../../layout/Container';
 
 export default function Home() {
+    const [repo, setRep] = useState([]);
+    const [input, setInput] = useState('');
+    console.log(repo);
+    useEffect(() => {
+        fetch('https://api.github.com/users/GuiLeoni14/repos')
+            .then((resp) => resp.json())
+            .then((data) => setRep(data))
+            .catch((err) => console.log(err));
+    }, []);
+
+    const handleInput = (e) => {
+        setInput(e.target.value);
+    };
     return (
         <Container>
-            <div>
-                <h1>Home</h1>
-            </div>
+            <h1>Home</h1>
+            <input type="text" onChange={handleInput} />
+            {repo && repo.map((repository, index) => <p key={index}>{repository.name}</p>)}
         </Container>
     );
 }
